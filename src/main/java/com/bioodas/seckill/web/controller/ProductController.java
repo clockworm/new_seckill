a3d18e51d0419e8972a575fbbbcde93424a0c150
TangLingYun <345684180@qq.com>
2018-01-16 23:36:34 +0800

    清除冗余代码

--- src/main/java/com/bioodas/seckill/web/controller/ProductController.java
+++ src/main/java/com/bioodas/seckill/web/controller/ProductController.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.spring4.context.SpringWebContext;
